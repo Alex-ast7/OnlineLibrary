dea594b865bf01f974804894ac159186ffbc661d
Лена <starostina.lena05@gmail.com>
2022-04-09 18:55:15 +0400

    Добавлены файлы, обеспечивающие работу с базой данных (сама бд тоже, но она пока пустая). В main.py добавлены функции для работы с зарегистрированными пользователями, изменен запуск приложения (чтобы потом с хероку проблем не было)

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, redirect
+
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from data import db_session
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'dnuiwy38noqmcxq8yr1FV&^npmNZB6ernm;s,c/'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route('/')
@@ -8,10 +30,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    db_session.global_init("db/onlineLibrary.db")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
